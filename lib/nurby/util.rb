dc1fb48beacf8897731e19ebc7622b9e79afcbcd
esotericpig <esotericpig@users.noreply.github.com>
2017-11-30 15:32:23 +0900

    Add reverse_chrs_case_equal?(...), more *int*?(), lower?()/upper?(), and some minor fixes

--- lib/nurby/util.rb
+++ lib/nurby/util.rb
@@ -25,13 +25,28 @@ require 'nurby/vars/set_var.rb'
 
 module Nurby
   module Util
+    def self.reverse_chrs_case_equal?(str1,str2)
+      i1 = str1.length
+      i2 = str2.length
+      
+      while (i1 -= 1) >= 0 && (i2 -= 1) >= 0
+        c1 = str1[i1]
+        c2 = str2[i2]
+        
+        # Equivalent to: (lower?(c1) && upper?(c2)) || (upper?(c1) && lower?(c2))
+        return false if lower?(c1) ^ lower?(c2)
+      end
+      
+      return true
+    end
+    
     def self.escape(str,escape_chr=ExpParser::DEFAULT_ESCAPE_CHR)
       chrs_to_escape = []
       var_classes = [RangeVar,SetVar]
       
       var_classes.each do |var_class|
-        chrs_to_escape.concat(var_class::CLOSING_TAG.chars)
-        chrs_to_escape.concat(var_class::OPENING_TAG.chars)
+        chrs_to_escape.concat(var_class::CLOSING_TAG.chars) if var_class.const_defined?(:CLOSING_TAG)
+        chrs_to_escape.concat(var_class::OPENING_TAG.chars) if var_class.const_defined?(:OPENING_TAG)
       end
       
       # escape(...) will create a Set of chrs
@@ -47,20 +62,35 @@ module Nurby
       return str
     end
     
-    def self.index_digit_or_letter(str)
-      return str.index(/[\da-zA-Z]/)
+    def self.index_int_or_letter(str)
+      return str.index(/[+\-\da-zA-Z]/)
     end
     
     def self.int?(str)
-      return str =~ /\A([[:space:]]*[+\-]?[[:space:]]*)([[:space:]]*\d+[[:space:]]*)+\z/
+      return (str =~ /\A([[:space:]]*[+\-]?[[:space:]]*)([[:space:]]*\d+[[:space:]]*)+\z/) ? true : false
+    end
+    
+    def self.int_or_word?(str)
+      return int?(str) || word?(str)
+    end
+    
+    def self.lower?(str)
+      # Faster than regex in my testing
+      return str == str.downcase()
     end
     
     def self.prefix?(chr)
-      return chr == '0' || chr == ' '
+      # Use regex so can allow '\t', etc.
+      return (chr =~ /\A(0|[[:space:]])\z/) ? true : false
+    end
+    
+    def self.upper?(str)
+      # Faster than regex in my testing
+      return str == str.upcase()
     end
     
     def self.whole_num?(str)
-      return str =~ /\A([[:space:]]*+?[[:space:]]*)([[:space:]]*\d+[[:space:]]*)+\z/
+      return (str =~ /\A([[:space:]]*+?[[:space:]]*)([[:space:]]*\d+[[:space:]]*)+\z/) ? true : false
     end
     
     def self.whole_num_or_word?(str)
@@ -68,7 +98,7 @@ module Nurby
     end
     
     def self.word?(str)
-      return str =~ /\A([[:space:]]*[a-zA-Z]+[[:space:]]*)+\z/
+      return (str =~ /\A([[:space:]]*[a-zA-Z]+[[:space:]]*)+\z/) ? true : false
     end
   end
 end

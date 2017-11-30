082e12d8d4bb37c607b35c08c8f7e42fdbb11b12
esotericpig <esotericpig@users.noreply.github.com>
2017-11-30 15:25:44 +0900

    Move VarFactory into own file

--- lib/nurby.rb
+++ lib/nurby.rb
@@ -24,43 +24,7 @@
 #       ['bb.com/',Var('[05-]'),' bb ',Var('[u=1-4]'),...]
 #       this is the job of the Parser class
 
-=begin
-
-Name = "Not cURl w/ ruBY"
-
-"bb.com/[05-] bb [u=1-4][l=1-8/u].pdf"
-
-"eu.com/[n=01-74]" -o "[n] eu [u=1-4][l=1-4/u]#{rb:1:r}[s=1-2/l].pdf"
---output_method "r:i('u','l'); e('{R*2}'); if v('u')%2 == 0 && v('l')%4 == 0 && v('s') == 2"
-
--o
--w --out-method
--m --in-method
-
-#{rb:1:r}
--1 = at beginning
- 0 = current place
- 1 = at end
-:: = default to 1
-
-{} = set
-[] = range
-#{ruby: ... }
-#{ruby::func}
--x (disable "#{}")
-
-rb = ruby alias
-i  = ignore()
-e  = evaluate()
-
-[l=1-8/u*2]
-{s=one,two/l*2}
-
-\{,\[,\# can all be escaped
-
-=end
-
-# vars/:    (x)Var       (x)RangeVar   (x)SetVar        ( )VarFactory
+# vars/:    (x)Var       (x)RangeVar   (x)SetVar        (x)VarFactory
 # methods/: ( )Method    ( )RubyMethod ( )MethodFactory
 # :         (x)ExpParser (x)ExpStr     (x)parser_errors
 #           ( )Parser    ( )Runner
@@ -79,53 +43,9 @@ require 'nurby/errors/parse_errors'
 require 'nurby/vars/range_var'
 require 'nurby/vars/set_var'
 require 'nurby/vars/var'
+require 'nurby/vars/var_factory'
 
 module Nurby
-  # TODO: catch ParseError and add var ID ($1)
-  module VarFactory
-    @@next_id = 1
-    @@vars = {}
-    
-    def self.check_vars
-      @@vars.each do |var|
-        if !@@vars.include?(var.per_var_id)
-          raise "Per var[#{var.per_var_id}] from var[#{var.id}] does not exist"
-        end
-      end
-    end
-    
-    def self.parse(exp_parser)
-      var = nil
-      
-      # FIXME: clone exp_parser before passing it to classes for savers
-      case exp_parser[0]
-      when '['
-        var = RangeVar.new(exp_parser)
-      when '{'
-        # Is not a method?
-        if exp_parser[-1] != '#'
-          var = SetVar.new(exp_parser)
-        end
-      end
-      
-      if !var.nil?
-        if var.id.nil? || var.id.empty?
-          var.id = "$#{@@next_id}"
-          @@next_id += 1
-        end
-        
-        if @@vars.include?(@var.id)
-          # TODO: raise error
-          raise
-        end
-        
-        @@vars[var.id] = var
-      end
-      
-      return var
-    end
-  end
-  
   class Method
     attr_accessor :eval
     attr_accessor :id
@@ -157,8 +77,15 @@ end
 begin
   exp = (ARGV.length > 0) ? ARGV[0] : '[l=1-4/u*2]'
   ep = Nurby::ExpParser.new(exp)
-  v = Nurby::RangeVar.new()
-  v.parse(ep)
+  vf = Nurby::VarFactory.new()
+  v = nil
+  
+  while ep.next_chr?()
+    pv = vf.parse!(ep)
+    v = pv if !pv.nil?
+  end
+  
+  vf.check_vars()
   
   puts "exp: #{exp}"
   puts "ep:\n#{ep}"
